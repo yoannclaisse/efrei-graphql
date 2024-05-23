c8b2258c1fb2af8ea910263c8a81416716a609a7
yoannclaisse <claisse.yoann@gmail.com>
2024-05-23 15:05:55 +0200

    add query todosByUserId

--- src/resolvers.todo.ts
+++ src/resolvers.todo.ts
@@ -15,87 +15,34 @@ export class TodoCreateInput {
 
 @Resolver(Todo)
 export class TodoResolver {
-  constructor(@Inject(PrismaService) private prismaService: PrismaService) {}
+  constructor(@Inject(PrismaService) private prismaService: PrismaService) { }
 
-  @ResolveField(() => User)
-  async user(@Root() todo: Todo): Promise<User | null> {
-    return this.prismaService.todo.findUnique({
+  // @ResolveField(() => User)
+  // async user(@Root() todo: Todo): Promise<User | null> {
+  //   return this.prismaService.todo.findUnique({
+  //     where: {
+  //       id: todo.id,
+  //     },
+  //   }).user();
+  // }
+
+  // @Query(() => Todo, { nullable: true })
+  // async todoById(@Args('id', { type: () => Int }) id: number) {
+  //   return this.prismaService.todo.findUnique({
+  //     where: { id },
+  //   });
+  // }
+
+  @Query(() => [Todo], { nullable: 'itemsAndList' })
+  async todosByUserId(@Args('userId', { type: () => Int }) userId: number): Promise<Todo[]> {
+    return this.prismaService.todo.findMany({
       where: {
-        id: todo.id,
+        userId,
       },
-    }).user();
-  }
-
-  @Query(() => Todo, { nullable: true })
-  async todoById(@Args('id', { type: () => Int }) id: number) {
-    return this.prismaService.todo.findUnique({
-      where: { id },
+      include: {
+        // Include the user in the response
+        user: true,
+      },
     });
   }
-
-  // @Query(() => [Todo])
-  // async todos(
-  //   @Args('searchString', { nullable: true }) searchString?: string,
-  //   @Args('skip', { nullable: true, type: () => Int }) skip?: number,
-  //   @Args('take', { nullable: true, type: () => Int }) take?: number,
-  //   @Args('orderBy', { nullable: true }) orderBy?: any,
-  // ): Promise<Todo[]> {
-  //   const or = searchString
-  //     ? {
-  //         OR: [
-  //           { title: { contains: searchString } },
-  //           { description: { contains: searchString } },
-  //         ],
-  //       }
-  //     : {};
-
-  //   return this.prismaService.todo.findMany({
-  //     where: {
-  //       ...or,
-  //     },
-  //     take: take || undefined,
-  //     skip: skip || undefined,
-  //     orderBy: orderBy || undefined,
-  //   });
-  // }
-
-  // @Mutation(() => Todo)
-  // async createTodo(
-  //   @Args('data') data: TodoCreateInput,
-  //   @Args('userId', { type: () => Int }) userId: number,
-  // ): Promise<Todo> {
-  //   return this.prismaService.todo.create({
-  //     data: {
-  //       title: data.title,
-  //       description: data.description,
-  //       user: {
-  //         connect: { id: userId },
-  //       },
-  //     },
-  //   });
-  // }
-
-  // @Mutation(() => Todo)
-  // async updateTodoCompletion(
-  //   @Args('id', { type: () => Int }) id: number,
-  //   @Args('completed', { type: () => Boolean }) completed: boolean,
-  // ): Promise<Todo> {
-  //   return this.prismaService.todo.update({
-  //     where: { id },
-  //     data: {
-  //       completed,
-  //     },
-  //   });
-  // }
-
-  // @Mutation(() => Todo, { nullable: true })
-  // async deleteTodo(
-  //   @Args('id', { type: () => Int }) id: number,
-  // ): Promise<Todo | null> {
-  //   return this.prismaService.todo.delete({
-  //     where: {
-  //       id,
-  //     },
-  //   });
-  // }
 }
